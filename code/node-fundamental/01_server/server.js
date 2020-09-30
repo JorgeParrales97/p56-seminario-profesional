bef037be446245299a3b9c04385e2b0d3c143070
JorgeParrales97 <jorrales.97@gmail.com>
2020-09-30 11:22:04 -0500

    Modificacion con la prueba con error

--- code/node-fundamental/01_server/server.js
+++ code/node-fundamental/01_server/server.js
@@ -8,14 +8,22 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(router)
 
-router.get('/Carrera', function(req, res){   
+router.get('/Carrera', function(req, res){  
+    console.log(req.headers) 
     res.send('Lista de carreras de la UPS')
 })
 
 router.post('/Carrera', function(req, res){  
-    console.log(req.body)
-    console.log(req.query) 
-    res.status(200).send({tipo_error:0, mensaje_error:'', mensaje_exito:'Añádido exitosamente'})
+
+    //console.log(req.body)
+    //console.log(req.query) 
+    //res.status(200).send({tipo_error:0, mensaje_error:'', mensaje_exito:'Añádido exitosamente'})
+    if(req.query.error == 'ok'){
+       res.status(500).send({tipo_error:1, mensaje_error:'Error en el servidor', mensaje_exito:''})
+    }else{
+        res.status(200).send({tipo_error:0, mensaje_error:'', mensaje_exito:'Todo esta bien'})
+    }
+    
 })
 
 app.use( '/', express.static('public'))
