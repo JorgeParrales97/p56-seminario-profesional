c9855bc0f58271fa5928f2f16b08d11e4a96b9d1
JorgeParrales97 <jorrales.97@gmail.com>
2020-10-01 18:36:10 -0500

    Modificaciones 01/10/2020

--- code/node-project/network/routes.js
+++ code/node-project/network/routes.js
@@ -1,13 +1,13 @@
 const express = require('express')
 //const carrera = require('../components/carrera/network')
-//const docente = require('../components/docente/network')
-const estudiante = require('../components/estudiante/network')
+const docente = require('../components/docente/network')
+//const estudiante = require('../components/estudiante/network')
 // const titulacion = require('../components/titulacion/network')
 
 const routes = function(server) {
  //  server.use('/Carrera', carrera)
-//server.use('/Docente', docente)
-server.use('/Estudiante', estudiante)
+server.use('/Docente', docente)
+//server.use('/Estudiante', estudiante)
 //    server.use('/titulacion', titulacion)
 }
 
