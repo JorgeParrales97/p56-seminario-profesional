df1adee0eef3a15e774cd6f5b1f17ee0dba329f0
JorgeParrales97 <jorrales.97@gmail.com>
2020-10-01 10:44:05 -0500

    Modificacion con la conexion con MongoDB

--- code/node-project/config.js
+++ code/node-project/config.js
@@ -1,5 +1,5 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://ups:ups2020@cluster0.pjyad.gcp.mongodb.net/utups?retryWrites=true&w=majority',
+    dbUrl: process.env.DB_URL || 'mongodb+srv://ups:ups2020@cluster0.jrgin.gcp.mongodb.net/utups?retryWrites=true&w=majority',
     port: process.env.PORT || 5000,
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE || '/',
